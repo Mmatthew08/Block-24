af94b5af85e0ab4854c5888977caca3ed8ffe34a
Matthew Eski <metehaneski08@gmail.com>
2023-11-03 13:18:44 -0500

    React and JSX

---
+++ demo-project/public/app.jsx
@@ -0,0 +1,14 @@
+import './App.css' ;
+import Navigation from './components/ Navigation' ;
+import Users from '../../components/users';
+
+function App(){
+    return(
+        <>
+        <Navigation/>
+        <h2>Block 24 Demo</h2>
+        <Users/>
+        </>
+    )
+}
+export default App
---
+++ notes.js
@@ -0,0 +1,14 @@
+//Intro to react
+// * React is a widely used and supported javascript framework 
+//* utilized by many companies worldwide. Front end Javascript library for building user interfaces.//*
+
+//What is JSX?: Javascript Syntax Extention. JSX looks like html but stricter and contain dynamic information.
+ // JSX and React are 2 different things. JSX is a syntax extention, React is a javascript library.
+
+ //*What is a hook?: Any technique that is used to change or add to the behavior
+//* of an application by intercepting function calls, or events passed between components. //*
+
+// The first, and most essential hook we learn is useState
+//What is useState?: 
+
+//Vite
